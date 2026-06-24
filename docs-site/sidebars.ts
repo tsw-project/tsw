ad19b841d769f5315fd51c2c4dc849c612af6463
bw0bw0 <bw0bw0@proton.me>
2026-06-24 14:58:25 +0200

    chore: add basic script documentation

--- docs-site/sidebars.ts
+++ docs-site/sidebars.ts
@@ -1,7 +1,17 @@
 import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
-  tutorialSidebar: ['intro', 'quickstart', 'cli'],
+  tutorialSidebar: [
+    'intro',
+    'quickstart',
+    {
+      type: 'category',
+      label: 'Usage',
+      collapsed: false,
+      items: ['globals', 'msw-class'],
+    },
+    'cli',
+  ],
 };
 
 export default sidebars;
